e07508350a66aa907bec6ad1f3f6db4cc66c0892
lachrist <lachrist@vub.ac.be>
2017-10-13 15:27:24 +0200

    npm

--- demo/main.js
+++ demo/main.js
@@ -5,32 +5,30 @@ const ChildSandbox = require("./child-sandbox.js");
 const div = document.createElement("div");
 document.body.appendChild(div);
 function kill (signal) { this.emit("exit", null, signal) }
+function noop () {}
+function autopush (chunk, encoding, callback) {
+  this.push(chunk, encoding);
+  callback();
+}
 const autopipe = () => new Stream.Duplex({
-  read: function () {},
+  read: noop,
   decodeStrings: false,
-  write: function (chunk, encoding, callback) {
-    this.push(chunk, encoding);
-    callback();
-  }
+  write: autopush
 });
 SandboxSpawner(div, ChildSandbox)((path, script, argv) => {
   const child = new Events();
   child.kill = kill;
   child.on("exit", () => { child.stdin.end() });
-  const process = {
+  child.stdin = autopipe();
+  child.stdout = autopipe();
+  child.stderr = autopipe();
+  const main = Function("process", script);
+  setTimeout(main, 0, {
     argv: ["node", path].concat(argv),
     exit: (code) => { child.emit("exit", code, null) },
-  };
-  child.stdin = process.stdin = autopipe();
-  child.stdout = process.stdout = autopipe();
-  child.stderr = process.stderr = autopipe();
-  setTimeout(() => {
-    try {
-      Function("process", script)(process);
-    } catch (error) {
-      process.stderr.write(error.stack+"\n", "utf8");
-      child.kill("SIGTERM");
-    }
-  }, 0);
+    stdin: child.stdin,
+    stdout: child.stdout,
+    stderr: child.stderr
+  });
   return child;
 });
