ed8644d67cfbd7c3ffb038747c894cacfc3cfbfa
lachrist <lachrist@vub.ac.be>
2017-10-13 13:48:52 +0200

    use auto-piped duplex stream in the demo

--- demo/main.js
+++ demo/main.js
@@ -5,27 +5,25 @@ const ChildSandbox = require("./child-sandbox.js");
 const div = document.createElement("div");
 document.body.appendChild(div);
 function kill (signal) { this.emit("exit", null, signal) }
-const pair = (writer, reader, name) => {
-  reader[name] = new Stream.Readable({read:()=>{}});
-  writer[name] = new Stream.Writable({
-    decodeStrings: false,
-    write: (chunk, encoding, callback) => {
-      reader[name].push(chunk, encoding);
-      callback();
-    }
-  });
-}
+const autopipe = () => new Stream.Duplex({
+  read: function () {},
+  decodeStrings: false,
+  write: function (chunk, encoding, callback) {
+    this.push(chunk, encoding);
+    callback();
+  }
+});
 SandboxSpawner(div, ChildSandbox)((path, script, argv) => {
   const child = new Events();
   child.kill = kill;
   child.on("exit", () => { child.stdin.end() });
   const process = {
     argv: ["node", path].concat(argv),
-    exit: (code) => { child.emit("exit", code, null) }
+    exit: (code) => { child.emit("exit", code, null) },
   };
-  pair(child, process, "stdin");
-  pair(process, child, "stdout");
-  pair(process, child, "stderr");
+  child.stdin = process.stdin = autopipe();
+  child.stdout = process.stdout = autopipe();
+  child.stderr = process.stderr = autopipe();
   setTimeout(() => {
     try {
       Function("process", script)(process);
